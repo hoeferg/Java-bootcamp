195dcbf8fd2d50f234a5e40fb815df2d7f3be21e
Gayle Hoefer <gaylehoefer@gmail.com>
2024-07-18 20:47:32 -0700

    keywords and expressions

---
+++ keeywrordsandExpressions.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        double kilometers = (100 * 1.609344); //This is a statement
+
+        int highScore = 50;
+        if (highScore > 25) {
+            highScore = 1000 + highScore; //There are two expressions highScire is one and the equation is the other
+        }
+        int health = 100;
+        if ((health < 25) && (highScore > 1000)) {
+            highScore = highScore - 1000;
+        }
+        //The following are expresstions
+        //There are 6 expressitions total
+        //Key words and ; do not count
+    }
+}
